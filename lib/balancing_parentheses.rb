059938ab84d4a36de8e6a3f2068348fbbaca696f
Kenneth Fitzgerald <fitzgeraldkd@gmail.com>
2021-11-27 14:58:38 -0500

    commit tree bde145c084390c2c87243113f6bd857019b9c2e6

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,17 @@
 require_relative './stack'
 
 # your code here
+def balancing_parentheses(str)
+  parentheses = Stack.new
+  count = 0
+  str.each_char do |c|
+    if c == '('
+      parentheses.push(c)
+    else
+      if parentheses.pop == nil
+        count += 1
+      end
+    end
+  end
+  count + parentheses.size
+end
